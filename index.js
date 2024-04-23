4b88e3006915fc64647288a5b97500a9270e020d
Emad Zaheer <87974481+emadzaheer@users.noreply.github.com>
2024-04-23 11:31:39 +0000

    commit deploy

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+require('dotenv').config()
 console.log("This is the starting file");
-
 const express = require('express')  //here we have made express an object, same as: import express from "express"    //common js vs module js
-const app = express()               //we'll use express using app variable
-const port = 3000                   // 65535 ports  
+const app = express()               //we'll use express using the app variable
+const port = 4000                   //65535 ports in total in a normal pc  
 
-app.get('/', (req, res) => {        //listen on port / send req and get res() 
+
+
+app.get('/', (req, res) => {        //listen on route '/' if someone sends get req and give res() which is hello world
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/twitter', (req, res) => {
+  res.send('ezs twitter')
+})
+
+app.get('/login', (req, res) => {
+  res.send('<h1>Login Page </h1>')
+} )
+
+app.get('/blockchain', (req, res) => {          //the server was running and this route gave error: said cannot get ... , to fix this simply restart.
+  res.send('<h2>VANAR CHAIN</h3> ')
+})
+
+app.listen(process.env.PORT, () => {                                        
+  console.log(`Example app listening on port ${process.env.PORT}`) 
 })
